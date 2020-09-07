0def3a6b5817896494757dea8fa10b973f139129
Leandro <leanter95@outlook.com>
2020-09-07 12:41:50 +0200

    update

--- bash/psDateLog.sh
+++ bash/psDateLog.sh
@@ -4,21 +4,20 @@
 #This variable stores the current date:
 currentDate=$(date +"%b %d %a %y")
 #This directory will save all the logs:
-mkdir .lgs 2>/dev/null
-#There is a directory for each user who runs this program:
-mkdir .lgs/${USER} 2>/dev/null
+mkdir /home/${USER}/.psDateLog 2>/dev/null
 #The log files are .txt files whose name is the current date.
-ls ./.lgs/${USER}/"$currentDate".txt 1>/dev/null 2>/dev/null
+ls /home/${USER}/.psDateLog/"$currentDate".txt 1>/dev/null 1>/dev/null 2>/dev/null
 if [ $? != 0 ]
 then
-echo >> ./.lgs/${USER}/"$currentDate".txt
-fi
+echo >> /home/${USER}/.psDateLog/"$currentDate".txt
 #Programs are logged every minute:
-echo -e "\n" >> ./.lgs/${USER}/"$currentDate".txt
-echo "At "$(date +%H:%M)": " >> ./.lgs/${USER}/"$currentDate".txt
-echo -e "\n" >> ./.lgs/${USER}/"$currentDate".txt
-echo $(ps -U ${USER} -o comm=) >> ./.lgs/${USER}/"$currentDate".txt
-rm -r ./.lgs/${USER}/.txt 2>/dev/null
+else
+echo " " >> /home/${USER}/.psDateLog/"$currentDate".txt
+echo "At "$(date +%H:%M)": " >> /home/${USER}/.psDateLog/"$currentDate".txt
+echo -e "\n" >> /home/${USER}/.psDateLog/"$currentDate".txt
+echo $(ps -U ${USER} -o comm=) >> /home/${USER}/.psDateLog/"$currentDate".txt
+rm -r /home/${USER}/.psDateLog/.txt 2>/dev/null
+fi
 #
 #You can run this script in background mode and set it to be executed during boot and every minute of the day through your System's settings.
 #
